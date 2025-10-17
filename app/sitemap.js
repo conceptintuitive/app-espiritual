8eea66f202244251ac10375c622e6f0d346775c4
BiancaChiabai <75100085+BiancaChiabai@users.noreply.github.com>
2025-10-17 11:36:10 -0300

    adicionar SEO completo

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://app-espiritual-psi.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ];
+}
